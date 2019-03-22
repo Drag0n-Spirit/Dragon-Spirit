2afba264e65b55d83669206ca2b08b3e1d3e18cb
IsaacMilarky <ikerobot@gmail.com>
2019-03-22 13:35:48 -0500

    gameobject done almost

--- Dragon Spirit/Dragon Spirit/GameObject.cpp
+++ Dragon Spirit/Dragon Spirit/GameObject.cpp
@@ -1,12 +1,58 @@
 #include "pch.h"
 #include "GameObject.h"
-#include <SFML/Graphics.hpp>
 
-GameObject::GameObject()
+
+GameObject::GameObject(sf::Sprite face, float x, float y)
 {
+	//Initialize values
+	object = face;
+	object.setOrigin(x, y);
+	position = sf::Vector2f(x, y);
+	velocity = sf::Vector2f(0, 0);
+
+	//Hitbox initially depends on sprite.
+	sf::FloatRect spriteBounds = object.getGlobalBounds();
+	hitbox = sf::RectangleShape();
+	//Size is the same as the size of the sprite.
+	hitbox.setSize(sf::Vector2f(spriteBounds.width, spriteBounds.height));
+	hitbox.setOrigin(x, y);
 }
 
+GameObject::GameObject(sf::Sprite face, sf::Vector2f pos)
+{
+	//Initialize values
+	object = face;
+	object.setOrigin(pos);
+	position = pos;
+	velocity = sf::Vector2f(0, 0);
+
+	//Hitbox initially depends on sprite.
+	sf::FloatRect spriteBounds = object.getGlobalBounds();
+	hitbox = sf::RectangleShape();
+	//Size is the same as the size of the sprite.
+	hitbox.setSize(sf::Vector2f(spriteBounds.width, spriteBounds.height));
+	hitbox.setOrigin(pos);
+}
 
 GameObject::~GameObject()
 {
+}
+
+void GameObject::update()
+{
+	//update position with velocity.
+	position.x += velocity.x;
+	position.y += velocity.y;
+
+	hitbox.move(velocity);
+	object.move(velocity);
+}
+
+void GameObject::collision(std::shared_ptr<GameObject> collisionObject)
+{
+}
+
+sf::RectangleShape GameObject::getHitbox()
+{
+	return hitbox;
 }
--- Dragon Spirit/Dragon Spirit/GameObject.h
+++ Dragon Spirit/Dragon Spirit/GameObject.h
@@ -1,18 +1,21 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <math.h>
 
 class GameObject
 {
 public:
-	GameObject();
+	GameObject(sf::Sprite,float,float);
+	GameObject(sf::Sprite, sf::Vector2f);
 	~GameObject();
 
 	virtual void update();
-	virtual void collision(GameObject);
+	virtual void collision(std::shared_ptr<GameObject>);
 
 	sf::RectangleShape getHitbox();
 
 private:
+	sf::Sprite object;
 	sf::RectangleShape hitbox;
 	sf::Vector2f position;
 	sf::Vector2f velocity;
