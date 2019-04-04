743c60e718c51fbfc922b5058243a5aa4300d2da
Drag0n-Spirit <47537982+Drag0n-Spirit@users.noreply.github.com>
2019-04-04 14:24:49 -0500

    Updates to game

--- Dragon Spirit/Dragon Spirit/Game.cpp
+++ Dragon Spirit/Dragon Spirit/Game.cpp
@@ -9,24 +9,72 @@ Game::Game(sf::RenderWindow *_window)
 }
 
 
-//
+//Start at an area beyond the first
 Game::Game(sf::RenderWindow *_window, int _area)
 {
 	window = _window;
 
 	area = _area;
+
+	//Spawn a dragon and give extra powers as appropriate
 }
 
 
-//
 Game::~Game()
 {
 }
 
 
-//Currently a stub
+//
+void Game::startScreen()
+{
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1))
+		gameState = _animation;
+
+	return;
+}
+
+
+//Handles pausing if game is in background, and calls functions based on gameState
 void Game::run()
 {
+	//Pause/unpause
+	if (event.type == sf::Event::LostFocus)
+		isPaused = true;
+	else if (event.type == sf::Event::GainedFocus)
+		isPaused = false;
+
+	//If not paused, do something based on the gameState
+	if (!isPaused)
+	{
+		//Clear window
+		window->clear();
+
+		//Drawing of shapes is done inside these functions
+		switch (gameState)
+		{
+		case _startScreen:
+			startScreen();
+			break;
+		case _running:
+			running();
+			break;
+		case _bossFight:
+			bossFight();
+			break;
+		case _animation:
+			animation();
+			break;
+		case _gameOver:
+			gameOver();
+			break;
+		case _endScreen:
+			endScreen();
+		}
+
+		//Display the window
+		window->display();
+	}
 
 	return;
 }
@@ -56,7 +104,7 @@ void Game::death()
 	lives -= 3;
 
 	if (lives <= -3)
-		gameState = gameOver;
+		gameState = _gameOver;
 	else
 	{
 		//Kill all non-dragon GameObjects
@@ -69,4 +117,5 @@ void Game::death()
 
 	return;
 }
+
 
--- Dragon Spirit/Dragon Spirit/Game.h
+++ Dragon Spirit/Dragon Spirit/Game.h
@@ -1,9 +1,7 @@
 #pragma once
 
-#include <SFML/Graphics.hpp>
-#include <time.h>
-#include <cstdlib>
 #include <vector>
+#include <SFML\Graphics.hpp>
 
 
 class Game
@@ -12,22 +10,35 @@ public:
 	Game(sf::RenderWindow *_window);
 	Game(sf::RenderWindow *_window, int _area);
 	~Game();
+	void startScreen();
+	void running();
+	void bossFight();
+	void animation();
+	void gameOver();
+	void endScreen();
 	void run();
 	void addScore(unsigned int scoreBonus);
-	void checkForSpawns();
+	//void checkForSpawns();
 	void gainLife();
 	void death();
 	//void spawnEnemy(SpawnData);
-	void spawnPowerup(int type = 0);
-	//void spawnProjectile(GameObject * source //the object that called the function);
-	void checkCollisions();
+	//void spawnPowerup(int type = 0);
+	/*
+	void spawnProjectile(GameObject * source, //the object that called the function
+	bool air = true, int fireType = 0);
+	*/
+	//void checkCollisions();
 private:
-	enum GameState {startScreen, running, bossFight, pause, animation, 
-		gameOver, endScreen};
-	sf::RenderWindow const * window;
+	enum GameState {
+		_startScreen, _running, _bossFight, _animation,
+		_gameOver, _endScreen
+	};
+	sf::RenderWindow * window;
 	sf::View view;
-	int gameState = startScreen;
-	int area = 1;
+	sf::Event event;
+	bool isPaused = false;
+	int gameState = _startScreen;
+	int area = 0;
 	int progress = 0;
 	int lives = 6;
 	int score = 0;
