f68bab2089ade6a415ff8ec84259729b998eef64
drewthomas16 <35775500+drewthomas16@users.noreply.github.com>
2019-04-12 14:10:08 -0500

    Enemy is looking good. It should be ready for actually enemies to be made.

--- Dragon Spirit/Dragon Spirit/Enemy.cpp
+++ Dragon Spirit/Dragon Spirit/Enemy.cpp
@@ -10,3 +10,18 @@ Enemy::Enemy()
 Enemy::~Enemy()
 {
 }
+
+
+int Enemy::getHit()
+{
+	if (hp <= 0) 
+	{ 
+		gamePtr->addScore(pointVal); 
+	}  
+
+	return hp;
+}
+
+
+bool Enemy::isHittable() { return hittable; }
+int Enemy::getType() { return type; }
--- Dragon Spirit/Dragon Spirit/Enemy.h
+++ Dragon Spirit/Dragon Spirit/Enemy.h
@@ -1,8 +1,18 @@
 #pragma once
-class Enemy
+#include "GameObject.h"
+
+
+class Enemy : public GameObject
 {
 public:
 	Enemy();
 	~Enemy();
+
+	virtual int getHit();
+	int getType();
+	bool isHittable();
+private:
+	int hp, type, pointVal = 0;
+	bool shiny, hittable;
 };
 
