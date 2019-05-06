a0b2bd12df9b366fd9a6db5009701f022186499a
IsaacMilarky <ikerobot@gmail.com>
2019-05-06 13:58:56 -0500

    Minor Comments

--- Dragon Spirit/Dragon Spirit/Animation.h
+++ Dragon Spirit/Dragon Spirit/Animation.h
@@ -4,6 +4,12 @@
 #include <vector>
 #include <fstream>
 
+/*
+	A simple class that loads a list of textures from a given directory.
+	Based on how often update() is called it spits out a usable texture.
+	Animation occurs because of the change in frames that it pushes.
+*/
+
 class Animation
 {
 public:
